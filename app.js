0032fc9f60d0baa0a1aa360c257dfbaa52b89f72
Jonathan Chen <jc1738593939@gmail.com>
2018-11-17 16:13:15 -0500

    added express

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var Sentiment = require('sentiment')
+
+var { getMenu } = require('./get_menu.js')
+
+const client_id = "VCRNKCHGA1ZH4ALEJ5TEZPGPUXVKWVPICA1M0J2KF5IBMJ33"
+const client_secret = "GPVYWMMB41WF4VM2JGPK030PXUTCQDHGJEIO30TYATWMV2IF"
+var lat = "33.416178808321426"
+var lon = "-111.872781158"
+var restaurant_name = "Starbucks"
+
+var app = express();
+
+app.get('/', async function (req, res) {
+	// Get restaurant menu
+	var menu = await getMenu(client_id, client_secret, restaurant_name, lat, lon)
+
+	// Analyze sentiment of reviews
+	var sentiment = new Sentiment()
+	// var result = sentiment.analyze(review)
+
+	res.send(menu);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
--- get_menu.js
+++ get_menu.js
@@ -1,11 +1,4 @@
-const client_id = "VCRNKCHGA1ZH4ALEJ5TEZPGPUXVKWVPICA1M0J2KF5IBMJ33"
-const client_secret = "GPVYWMMB41WF4VM2JGPK030PXUTCQDHGJEIO30TYATWMV2IF"
-var lat = "33.416178808321426"
-var lon = "-111.872781158"
-var search_url = "https://api.foursquare.com/v2/venues/search?limit=1&ll=" + lat + "," + lon
-var restaurant_name = "Starbucks"
-search_url += "&query=" + encodeURI(restaurant_name)
-search_url += "&client_id=" + client_id + "&client_secret=" + client_secret + "&v=20181116"
+const request = require("request-promise")
 
 /** Example to test
  * "name":"Starbucks","latitude":33.4165848044,"longitude":-111.872781158
@@ -13,58 +6,73 @@ search_url += "&client_id=" + client_id + "&client_secret=" + client_secret + "&
  */
 
 // Get restaurant menu given name, latitude, and longitude
-var venue_id = null;
-fetch(search_url)
-	.then(response => {
-		return response.json()
-	})
-	.then(response => {
-		if (response.meta.code == 200) {
-			// success
-			venue_id = response.response.venues[0].id
+async function getMenu(client_id, client_secret, restaurant_name, lat, lon) {
+	var venue_id = null;
 
-			return venue_id
+	try {
+		// Search for venue
+		var results = await request({
+			url: "https://api.foursquare.com/v2/venues/search",
+			method: "GET",
+			qs: {
+				client_id: client_id,
+				client_secret: client_secret,
+				v: "20181116",
+				query: encodeURI(restaurant_name),
+				limit: 1,
+				ll: lat + "," + lon
+			}
+		}).json()
+
+		if (!results.meta) {
+			throw new Error("Error with request")
+		}
+
+		// Parse venue id
+		if (results.meta.code == 200) {
+			venue_id = results.response.venues[0].id
 		} else {
 			throw new Error("Error fetching venue")
 		}
-	})
-	.then(venue_id => {
-		// get menu
+
+		// Get menu
 		var menu_url = "https://api.foursquare.com/v2/venues/" + venue_id + "/menu"
 		menu_url += "?client_id=" + client_id + "&client_secret=" + client_secret + "&v=20181116"
-		
-		fetch(menu_url)
-			.then(response => {
-				return response.json()
-			})
-			.then(response => {
-				if (response.meta.code == 200) {
-					// success
-					var menu = response.response.menu.menus.items
-					var menu_items = []		// all menu items
 
-					// iterate through menu to get items
-					if (menu.length > 0) {
-						for (var i = 0; i < menu.length; i++)
-							for (var j = 0; j < menu[i].entries.count; j++)
-								for (var k = 0; k < menu[i].entries.items[j].entries.count; k++)
-									menu_items.push(menu[i].entries.items[j].entries.items[k].name)
-							
-						//console.log(menu_items)
-						return menu_items
-					} else {
-					// Menu doesn't exist
-					throw new Error("No menu available")
-					}
-				} else {
-					// Couldn't make request
-					throw new Error("Error fetching menu")
-				}
-			})
-			.then(menu_items => {
-				console.log(menu_items)
-			})
-	})
-	.catch(error => {
-		console.log(error.message)
-	})
+		results = await request({
+			url: menu_url,
+			method: "GET"
+		}).json()
+
+		if (!results.meta) {
+			throw new Error("Error with request")
+		}
+
+		// Parse menu items
+		if (results.meta.code == 200) {
+			var menu = results.response.menu.menus.items
+			var menu_items = []		// all menu items
+
+			//iterate through menu to get items
+			if (menu.length > 0) {
+				for (var i = 0; i < menu.length; i++)
+					for (var j = 0; j < menu[i].entries.count; j++)
+						for (var k = 0; k < menu[i].entries.items[j].entries.count; k++)
+							menu_items.push(menu[i].entries.items[j].entries.items[k].name)
+				
+				return menu_items
+			} else {
+				throw new Error("No menu available")
+			}
+		} else {
+			throw new Error("Error fetching menu")
+		}		
+	}
+	catch (error) {
+		console.log(error)
+	}
+}
+
+module.exports = {
+	getMenu
+}
